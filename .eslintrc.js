d1ba07fb26a2f98054126719723e911e4d80f863
Cjiaye <11204115+cjiaye@user.noreply.gitee.com>
2022-07-05 09:20:04 +0800

    docs(all): all

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: 'error',
+    quotes: 'warn', // 修改为警告
+    quotes: 'off', // 修改不校验
+    'space-before-function-paren': 'off',
+    'vue/multi-word-component-names': 'off'
+  }
+}
